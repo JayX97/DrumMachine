da3d0aeca307144fc8323b80903d60dfb3281fbe
JayX97 <jason.fernandezx@yahoo.com>
2024-09-11 05:15:25 -0400

    Created function to play unique audio clip when clicking drum pad

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -2,13 +2,17 @@ const DrumMachine = (props) => {
     const drumPads = props.drumPads;
     const displayText = "test";
 
+    const playAudio = (audio) => {
+        document.getElementById(audio).play();
+    }
+
     return (
         <div id="drum-machine">
             <div id="display">{displayText}</div>
             {
                 drumPads.map(key => {
                     return (
-                        <button className="drum-pad" id={key.name}>
+                        <button onClick={() => playAudio(key.key)} className="drum-pad" id={key.name}>
                             <audio src={key.link} className="clip" id={key.key} />
                             {key.key}
                         </button>
